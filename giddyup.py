138770e94aa1cef3e838c0dbe9933cc651a80e87
GiddyUp <giddyup@roku.com>
2015-06-30 18:26:19 -0700

    asdfasdf

--- giddyup.py
+++ giddyup.py
@@ -17,3 +17,7 @@ print gPull
 CMD = "git pull"
 gPull = subprocess.check_output(CMD, stderr=subprocess.STDOUT, shell=True)
 print gPull
+
+CMD = "s3cmd sync . s3://ods-working/git/"
+gPull = subprocess.check_output(CMD, stderr=subprocess.STDOUT, shell=True)
+print gPull
