ab62fdf2e97e358f682ede2f2940327edb0075c8
GiddyUp <giddyup@roku.com>
2015-06-30 17:48:24 -0700

    sdfasdf asd fasdf

--- giddyup.py
+++ giddyup.py
@@ -6,6 +6,14 @@ CMD = "git config --global push.default simple"
 gPull = subprocess.check_output(CMD, stderr=subprocess.STDOUT, shell=True)
 print gPull
 
-CMD = "git push"
+CMD = "git config --global user.name 'GiddyUp'"
+gPull = subprocess.check_output(CMD, stderr=subprocess.STDOUT, shell=True)
+print gPull
+
+CMD = "git config --global user.email giddyup@roku.com"
+gPull = subprocess.check_output(CMD, stderr=subprocess.STDOUT, shell=True)
+print gPull
+
+CMD = "git pull"
 gPull = subprocess.check_output(CMD, stderr=subprocess.STDOUT, shell=True)
 print gPull
